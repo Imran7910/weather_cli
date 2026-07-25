0dab499815081d2edea823ce11c2180ce5b642eb
Imran7910 <imranmunnashaik@gmail.com>
2026-07-25 21:30:29 +0530

    user friendly output

--- weather.py
+++ weather.py
@@ -9,9 +9,16 @@ response=requests.get(request_url)
 
 if response.status_code == 200:
     data=response.json()
+    city_name=data['name']
     temperature=data['main']['temp']
     humidity=data['main']['humidity']
     weather_description = data['weather'][0]['description']
-    print(f"Temperature: {temperature}K,Humidity: {humidity}%, Description: {weather_description}")
+    print()
+    print(f"Weather in {city_name}:")
+    print("-" * 20)
+    print(f"Temperature: {temperature}K")
+    print(f"Humidity: {humidity}%")
+    print(f"Conditions: {weather_description.capitalize()}")
+    print()
 else:
     print(f"Error: the request is failed with status code {response.status_code}")
