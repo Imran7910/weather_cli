9ea05c162526c3ac31f4ffacba97c39df5f47b59
Imran7910 <imranmunnashaik@gmail.com>
2026-07-27 23:08:04 +0530

    added a try and except block for error handling

--- weather.py
+++ weather.py
@@ -1,24 +1,28 @@
 import requests
 import json
+import sys
 BASE_URL="https://api.openweathermap.org/data/2.5/weather"
 API_KEY="19e9712d60f01cc61eb6571c9ba428fb"
 CITY="london"
 
 request_url = f"{BASE_URL}?q={CITY}&appid={API_KEY}"
-response=requests.get(request_url)
-
-if response.status_code == 200:
-    data=response.json()
-    city_name=data['name']
-    temperature=data['main']['temp']
-    humidity=data['main']['humidity']
-    weather_description = data['weather'][0]['description']
-    print()
-    print(f"Weather in {city_name}:")
-    print("-" * 20)
-    print(f"Temperature: {temperature}K")
-    print(f"Humidity: {humidity}%")
-    print(f"Conditions: {weather_description.capitalize()}")
-    print()
-else:
-    print(f"Error: the request is failed with status code {response.status_code}")
+try:
+    response=requests.get(request_url)
+    if response.status_code == 200:
+        data=response.json()
+        city_name=data['name']
+        temperature=data['main']['temp']
+        humidity=data['main']['humidity']
+        weather_description = data['weather'][0]['description']
+        print()
+        print(f"Weather in {city_name}:")
+        print("-" * 20)
+        print(f"Temperature: {temperature}K")
+        print(f"Humidity: {humidity}%")
+        print(f"Conditions: {weather_description.capitalize()}")
+        print()
+    else:
+        print(f"Error: the request is failed with status code {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print(f"An error occured: {e}")
+    sys.exit(1)
